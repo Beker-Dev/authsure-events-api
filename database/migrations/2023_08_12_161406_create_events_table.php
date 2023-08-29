cddd2754d6725653bbcd38298c0d1facd2f9cc3c
Gabriel Fidelis Pereira <83375130+FidelisGf@users.noreply.github.com>
2023-08-28 21:12:40 -0300

    changes

--- database/migrations/2023_08_12_161406_create_events_table.php
+++ database/migrations/2023_08_12_161406_create_events_table.php
@@ -13,13 +13,12 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('events', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('type', 50)->nullable(false);
+            $table->string('action', 50)->nullable(false);
+            $table->integer('type_id')->nullable(false);
+            $table->integer('session_id')->nullable(false);
             $table->timestamps();
         });
     }
@@ -31,6 +30,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('events');
     }
 };
